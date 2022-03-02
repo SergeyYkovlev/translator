b6763a6d47892f9cc537882c9d1a442666396f7d
SergeyYkovlev <yakovlevserg09@gmail.com>
2022-03-02 16:59:35 +0600

    Fix review issue

--- Classes/Presentation/Modules/SearchViewController.swift
+++ Classes/Presentation/Modules/SearchViewController.swift
@@ -27,6 +27,5 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
     }
 }
